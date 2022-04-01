afd19a12b89c5c64aad2f24b41a599b4d27584ae
Misrat121 <you@example.com>
2022-04-01 11:09:39 +0600

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import OrderReview from './Components/OrderReview/OrderReview';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+     <Routes>
+       <Route path="/" element={<Home></Home>}></Route>
+       <Route path="/home" element={<Home></Home>}></Route>
+       <Route path="/orderreview" element={<OrderReview></OrderReview>}></Route>
+     </Routes>
     </div>
   );
 }
